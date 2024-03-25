275a3e821e329a1c3052fd51d3280ee3e7fa86e0
jwathika <wathika02@gmail.com>
2024-03-25 09:37:23 +0300

    grader_done

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('Welcome');
+//alert('welcome');
+function calculateGrade() {
+	let marks = document.getElementById('marks').value;
+	let grade;
+	//ensure it's a number and within the range
+	if (isNaN(marks) || marks < 0 || marks > 100) {
+		$('#errorModal').modal('show');
+		return;
+	}
+	if (marks > 79) {
+		grade = 'A';
+	} else if (marks >= 60 && marks <= 79) {
+		grade = 'B';
+	} else if (marks >= 50 && marks <= 59) {
+		grade = 'C';
+	} else if (marks >= 40 && marks <= 49) {
+		grade = 'D';
+	} else {
+		grade = 'E';
+	}
+
+	document.getElementById('gradeResult').innerHTML =
+		'<p>Grade: ' + grade + '</p>';
+}
